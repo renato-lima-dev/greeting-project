5a9ab126ae0ab743cb48536f520800d40c7cfe86
renato <renatolima.prog@gmail.com>
2023-08-16 17:33:15 -0400

    feat:create project

---
+++ src/main/java/ifam/edu/com/GreetingProjectApplication.java
@@ -0,0 +1,13 @@
+package ifam.edu.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ifam/edu/com/GreetingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ifam.edu.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
