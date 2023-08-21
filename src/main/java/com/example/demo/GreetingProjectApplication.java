6c7356ef79345fd83f9fe38e11025d356c88b756
renato lima <renatolima.prog@gmail.com>
2023-08-21 17:32:47 -0400

    create project

---
+++ src/main/java/com/example/demo/GreetingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingProjectApplication.class, args);
+	}
+
+}
